3d22990e1f444f12c8317e61f25e6ee124b47112
CyberRecrent <alen.moldabaev@mail.ru>
2026-03-28 18:31:25 +0500

    first commit

---
+++ app/main.py
@@ -0,0 +1,10 @@
+print("HEllo, Tis is my pet-project")
+
+Name = input("Your name: ")
+Second_name = input("Your second name: ")
+Age = int(input("Your age: "))
+Mail = input("Email")
+Subscribe = input("Your plan")
+
+if Subscribe == "Pro":
+    print(f"Dear {Name}, {Second_name} you have Pro subscribe, you can go boxing, gym")
