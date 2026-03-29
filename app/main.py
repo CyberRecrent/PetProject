bcfb876550cf7430c269aefcc63c4cb60e7025a6
CyberRecrent <alen.moldabaev@mail.ru>
2026-03-29 19:10:27 +0500

    Added end

--- app/main.py
+++ app/main.py
@@ -7,4 +7,10 @@ Mail = input("Email")
 Subscribe = input("Your plan")
 
 if Subscribe == "Pro":
-    print(f"Dear {Name}, {Second_name} you have Pro subscribe, you can go boxing, gym")
+    print(f"Dear {Name} {Second_name} you have Pro subscribe, you can go boxing, gym")
+elif Subscribe == "Standart":
+    print(f"Dear {Name} {Second_name} you can go gym. If you want boxing please upgrade your Subscribe")
+else:
+    print("Please buy subscribtion. For more information you can address to our support")
+
+print("Thanks for using app")
